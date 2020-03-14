0f37455de9897b55f8d9e6b22609c50c6bcae3dd
MackBai <13661112843@163.com>
2020-03-14 14:27:55 +0800

    初始化

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
+  presets: ['@vue/cli-plugin-babel/preset'],
+  plugins: [ // 按需引入
+    ['import', {
+      libraryName: 'vant',
+      libraryDirectory: 'es',
+      style: true
+    }]
   ]
 }
