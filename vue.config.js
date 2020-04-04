21633510a2bfe039195060401ec4398578d2a872
MackBai <13661112843@163.com>
2020-04-04 16:41:44 +0800

    日常修复02

--- vue.config.js
+++ vue.config.js
@@ -23,6 +23,13 @@
 //   })
 // })()
 module.exports = {
+  chainWebpack: (config) => {
+    const imagesRule = config.module.rule('images')
+    imagesRule
+      .use('url-loader')
+      .loader('url-loader')
+      .tap(options => Object.assign(options, { limit: 6144 }))
+  },
   devServer: {
     open: true,
     // host: 'localhost',
